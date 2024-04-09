afa1e9dab6e13fb0d647c83cbd5794722d2d2cbd
Harshit Jain <jharshit436@gmail.com>
2024-04-09 20:07:02 +0530

    add requriments.txt

--- config/db.py
+++ config/db.py
@@ -1,8 +1,8 @@
-import env
+
 from pymongo import MongoClient
 
 
-MONGO_URI = env.ATLAS_URL
+MONGO_URI = "mongodb+srv://contactjharshit:Harshit123@notetakingapp.5g9ov6g.mongodb.net/?retryWrites=true&w=majority&appName=notetakingapp"
 
 conn = MongoClient(MONGO_URI)
 
